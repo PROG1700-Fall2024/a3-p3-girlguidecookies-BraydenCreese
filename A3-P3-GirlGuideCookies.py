0e7a5e008cf86703b723700acf9bdb63ba8dcf7a
BraydenCreese <kidson2017@gmail.com>
2024-11-21 21:41:41 -0800

    Done. 1 Day Late

--- A3-P3-GirlGuideCookies.py
+++ A3-P3-GirlGuideCookies.py
@@ -4,19 +4,44 @@
 #               those guides who go above and beyond in their sales efforts. The organizers
 #               want a program to print the guide list and their prizes.
 
-#Student #:     
-#Student Name:  
+#Student #: Brayden Creese 
+#Student Name: W0491583
 
-def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
+def girlGuide():
+
+    while True:
+        try:
+            numOfGuides = int(input("Enter the number of guides selling cookies: "))
+            break
+        except ValueError:
+            print("Invalid Input! Please enter a valid number for 'number of guides'.")
+
+    guides = []
+
+    for i in range(numOfGuides):
+        name = input(f"\nEnter the name of guide #{i + 1}: ")
+        sales = int(input(f"Enter the number of boxes sold by {name}: "))
+        guides.append({"name": name, "sales": sales})
+
+    totalSales = sum(guide["sales"] for guide in guides)
+    avgSales = totalSales / numOfGuides
+    
+    max_sales = max(guide["sales"] for guide in guides)
+    for guide in guides:
+        if guide["sales"] == max_sales:
+            guide["prize"] = "- Trip to Girl Guide Jamboree in Aruba!"
+        elif guide["sales"] > avgSales:
+            guide["prize"] = "- Super Seller Badge"
+        elif guide["sales"] > 0:
+            guide["prize"] = "- Left over cookies"
+        else:
+            guide["prize"] = "-"
+    
+    print("\nThe average number of boxes sold by each guide was {:.1f}".format(avgSales))
+    print("\nGuide\t\tPrizes Won:")
+    print("=" * 40)
+    for guide in guides:
+        print(f"{guide['name']}\t\t{guide['prize']}")
 
 
-
-
-
-
-
-
-    # YOUR CODE ENDS HERE
-
-main()
+girlGuide()
